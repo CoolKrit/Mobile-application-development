000589467f24963c08739f353820eaf12d806f39
Amir Makhmudov <amir-makhmudov2004@mail.ru>
2023-02-24 12:54:49 +0300

    Added SplashScreen with all the necessary components

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_in);
 
         Toast.makeText(MainActivity.this, "onCreate", duration).show();
         Log.i(TAG, "onCreate");
---
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    sleep(2000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                }
+            }
+        };
+        thread.start();
+    }
+}
